0096b706e965763f8dcec6f2c94326492de4d1ba
Hendra Permana <hynra@yahoo.com>
2016-12-19 14:02:58 +0700

    revert, remove starter kit

--- index.android.js
+++ index.android.js
@@ -1,5 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
 
+import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
-import setup from './js/setup';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Icon } from 'native-base';
 
-AppRegistry.registerComponent('NativeStarterKit', setup);
+export default class welldone extends Component {
+    render() {
+        return (
+            <Container>
+                <Header>
+                    <Title>Header</Title>
+                </Header>
+
+                <Content>
+
+                </Content>
+
+                <Footer>
+                    <FooterTab>
+                        <Button transparent>
+                            <Icon name='ios-call' />
+                        </Button>
+                    </FooterTab>
+                </Footer>
+            </Container>
+        );
+    }
+}
+
+
+AppRegistry.registerComponent('welldone', () => welldone);
