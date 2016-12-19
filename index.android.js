6e1c17e1e5000e50e8101ffd926ee8994d795f28
Hendra Permana <hynra@yahoo.com>
2016-12-19 12:54:38 +0700

    test native starter kit

--- index.android.js
+++ index.android.js
@@ -1,36 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
-import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Icon } from 'native-base';
+import setup from './js/setup';
 
-export default class welldone extends Component {
-    render() {
-        return (
-            <Container>
-                <Header>
-                    <Title>Header</Title>
-                </Header>
-
-                <Content>
-
-                </Content>
-
-                <Footer>
-                    <FooterTab>
-                        <Button transparent>
-                            <Icon name='ios-call' />
-                        </Button>
-                    </FooterTab>
-                </Footer>
-            </Container>
-        );
-    }
-}
-
-
-AppRegistry.registerComponent('welldone', () => welldone);
+AppRegistry.registerComponent('NativeStarterKit', setup);
